624088f443db779bc93d79057177a32065679e5d
Matheus Mota <devmatheusmota@gmail.com>
2022-08-31 19:42:35 -0300

    LoginRouter logs errors to an external file

--- src/presentation/routers/login-router.js
+++ src/presentation/routers/login-router.js
@@ -1,28 +1,29 @@
 const HttpResponse = require('../helpers/http-response');
+const fs = require('fs');
 module.exports = class LoginRouter {
 	constructor(authUseCase) {
 		this.authUseCase = authUseCase;
 	}
 	route(httpRequest) {
-		if (
-			!httpRequest ||
-			!httpRequest.body ||
-			!this.authUseCase ||
-			!this.authUseCase.auth
-		) {
+		try {
+			const { email, password } = httpRequest.body;
+			if (!email) {
+				return HttpResponse.badRequest('email');
+			}
+			if (!password) {
+				return HttpResponse.badRequest('password');
+			}
+			const accessToken = this.authUseCase.auth(email, password);
+			if (!accessToken) {
+				return HttpResponse.unauthorizedError();
+			}
+			return HttpResponse.ok({ accessToken });
+		} catch (error) {
+			const date = new Date();
+			fs.appendFile('error-log.txt', `${date} ${error}\n`, (err) => {
+				if (err) throw err;
+			});
 			return HttpResponse.serverError();
 		}
-		const { email, password } = httpRequest.body;
-		if (!email) {
-			return HttpResponse.badRequest('email');
-		}
-		if (!password) {
-			return HttpResponse.badRequest('password');
-		}
-		const accessToken = this.authUseCase.auth(email, password);
-		if (!accessToken) {
-			return HttpResponse.unauthorizedError();
-		}
-		return HttpResponse.ok({ accessToken });
 	}
 };
