a4030a84f9ca807d504cd01db4998840a237c92e
Matheus Mota <devmatheusmota@gmail.com>
2022-10-05 15:32:12 -0300

    Ensure TokenGenerator calls JWT with correct values

--- src/utils/helpers/token-generator.spec.js
+++ src/utils/helpers/token-generator.spec.js
@@ -2,20 +2,24 @@ import { describe, it, expect } from 'vitest';
 const jwt = require('jsonwebtoken');
 const jwtSpy = require('../../../__mocks__/jsonwebtoken');
 class TokenGenerator {
+	constructor(secret) {
+		this.secret = secret;
+	}
 	async generate(id) {
-		return jwt.sign(id, 'secret');
+		this.id = id;
+		return jwt.sign(id, this.secret);
 	}
 }
 
 const makeSut = () => {
-	return new TokenGenerator();
+	return new TokenGenerator('secret');
 };
 
 describe('Token Generator', () => {
 	it('Should return null if JWT returns null', async () => {
 		const sut = makeSut();
 		jwtSpy.token = null;
-		const token = await sut.generate('any_id');
+		await sut.generate('any_id');
 		expect(jwtSpy.token).toBeNull();
 	});
 
@@ -24,5 +28,13 @@ describe('Token Generator', () => {
 		const token = await sut.generate('any_id');
 		jwtSpy.token = token;
 		expect(token).toBe(jwtSpy.token);
+	});
+
+	it('Should call JWT with correct values', async () => {
+		const sut = makeSut();
+		await sut.generate('any_id');
+		jwtSpy.sign('any_id', sut.secret);
+		expect(jwtSpy.id).toBe(sut.id);
+		expect(jwtSpy.secret).toBe(sut.secret);
 	});
 });
