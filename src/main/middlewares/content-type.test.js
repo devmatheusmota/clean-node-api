eabef461c018e9b360158cbfcd75096a8b5479a3
Matheus Mota <devmatheusmota@gmail.com>
2022-10-07 10:52:20 -0300

    Ensure requests return content-type as XML if forced

--- src/main/middlewares/content-type.test.js
+++ src/main/middlewares/content-type.test.js
@@ -3,10 +3,20 @@ const request = require('supertest');
 const app = require('../config/app');
 
 describe('Content-Type Middleware', () => {
-	it('Should return JSON content type as default', async () => {
+	it('Should return JSON content-type as default', async () => {
 		app.get('/test_content_type', (req, res) => {
 			res.send('');
 		});
 		await request(app).get('/test_content_type').expect('Content-Type', /json/);
+	});
+
+	it('Should return XML content-type if forced', async () => {
+		app.get('/test_content_type_xml', (req, res) => {
+			res.type('xml');
+			res.send('');
+		});
+		await request(app)
+			.get('/test_content_type_xml')
+			.expect('Content-Type', /xml/);
 	});
 });
