e57b4f75975e9e6315cf37ac544ffb26f572806b
Matheus Mota <devmatheusmota@gmail.com>
2022-10-07 10:23:10 -0300

    Enabled CORS

--- src/main/config/setup.test.js
+++ src/main/config/setup.test.js
@@ -4,10 +4,20 @@ const app = require('./app')
 
 describe('App Setup', () => {
   it('Shoul disable x-powered-by header', async () => {
-    app.get('/test', (req, res) => {
+    app.get('/test_x-powered-by', (req, res) => {
+      res.send('')
+    })
+    const res = await request(app).get('/test_x-powered-by')
+    expect(res.headers['x-powered-by']).toBeUndefined()
+  });
+
+  it('Shoul enable CORS', async () => {
+    app.get('/test_cors', (req, res) => {
       res.send('')
     })
     const res = await request(app).get('/test')
-    expect(res.headers['x-powered-by']).toBeUndefined()
+    expect(res.headers['access-control-allow-origin']).toBe('*')
+    expect(res.headers['access-control-allow-methods']).toBe('*')
+    expect(res.headers['access-control-allow-headers']).toBe('*')
   });
 });
