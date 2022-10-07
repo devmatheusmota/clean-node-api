94ffef8cfd97b3bb4fb410e8e521c3a23c4627f2
Matheus Mota <devmatheusmota@gmail.com>
2022-10-07 11:22:08 -0300

    Ensure requests return content-type as XML if forced

--- src/main/middlewares/content-type.test.js
+++ src/main/middlewares/content-type.test.js
@@ -1,7 +1,6 @@
-import { describe, it, expect } from 'vitest';
+import { describe, it } from 'vitest';
 const request = require('supertest');
 const app = require('../config/app');
-
 describe('Content-Type Middleware', () => {
 	it('Should return JSON content-type as default', async () => {
 		app.get('/test_content_type', (req, res) => {
